618fe7215c3c97c5dfcc2b4291ffd66b6d3ba738
iyoubee <al.ayubi2020@gmail.com>
2024-05-23 11:01:57 +0700

    back to firebase

--- savePrediction.mjs
+++ savePrediction.mjs
@@ -1,20 +1,19 @@
-import { Firestore } from '@google-cloud/firestore'
+import admin from 'firebase-admin'
 import serviceAccount from './service.json' assert { type: 'json' }
 
-// Initialize Firestore client with service account credentials
-const firestore = new Firestore({
-  projectId: serviceAccount.project_id,
-  credentials: {
-    client_email: serviceAccount.client_email,
-    private_key: serviceAccount.private_key,
-  },
+// Inisialisasi Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://submissionmlgc-hilman-424200.firebaseio.com',
 })
 
-// Function to save prediction to Firestore
+// Fungsi untuk menyimpan prediksi ke Firestore
 async function savePredictionToFirestore(prediction) {
   try {
-    // Add prediction to the 'predictions' collection
-    await firestore.collection('predictions').doc(prediction.id).set({
+    const db = admin.firestore()
+
+    // Tambahkan prediksi ke koleksi 'predictions'
+    await db.collection('predictions').doc(prediction.id).set({
       id: prediction.id,
       result: prediction.result,
       suggestion: prediction.suggestion,
